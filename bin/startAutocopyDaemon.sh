8cb0119dd73a0942eb741bacf3291871f96fc0b0
Nathan Hammond <nhammond@stanford.edu>
2013-08-27 12:19:57 -0700

    start script sets folder permissions before launching autocopy, not after.

--- bin/startAutocopyDaemon.sh
+++ bin/startAutocopyDaemon.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-launchctl load AutocopyDaemon.plist
-launchctl start AutocopyDaemon
-
 for dir in /Volumes/IlluminaRuns?
 do 
 chmod 770 $dir;
 chown illumina:staff $dir;
 chown illumina:staff $dir/Runs;
 done
+
+launchctl load AutocopyDaemon.plist
+launchctl start AutocopyDaemon
